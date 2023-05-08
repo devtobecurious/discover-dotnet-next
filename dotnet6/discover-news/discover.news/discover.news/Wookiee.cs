fa5370c5e26044c32e686cc2b67cedafaec183d6
Evan BOISSONNOT <evan@boissonnot.fr>
2023-05-08 11:38:59 +0200

    feat: nullable

---
+++ dotnet6/discover-news/discover.news/discover.news/Wookiee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discover.news
+{
+    internal class Wookiee
+    {
+        string? name;
+
+        public Wookiee(string name)
+        {
+            this.Name = name;
+        }
+
+        [return: MaybeNull]
+        string GetName()
+        {
+            return null;
+        }
+
+        [return: NotNullIfNotNull(nameof(name))]
+        public string? GetState(string? name)
+        {
+            return null;
+        }
+
+        public string? Name { get => name; set => name = value ?? throw new ArgumentNullException("Value not set"); }
+
+        [MaybeNull]
+        public string Surname { get; set; }
+    }
+}
