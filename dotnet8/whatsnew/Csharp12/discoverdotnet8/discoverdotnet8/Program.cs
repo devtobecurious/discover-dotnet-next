1ffda9857cd2380c7862195508da1f2301b4c2a2
Evan BOISSONNOT <evan@boissonnot.fr>
2023-09-08 18:51:54 +0200

    feat: spread and collection expression

--- dotnet8/whatsnew/Csharp12/discoverdotnet8/discoverdotnet8/Program.cs
+++ dotnet8/whatsnew/Csharp12/discoverdotnet8/discoverdotnet8/Program.cs
@@ -8,3 +8,29 @@ Console.WriteLine(point.value);
 
 int[] positions = [1, 2, 3];
 Span<int> values = [1, 2, 3];
+
+//var array = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+
+
+
+//static void Generic<T>(Span<T> value) { }
+static void Generic<T>(T[] value) { }
+Generic([1, 2, 3]);
+
+//static void SpanDerived(Span<string> value) { }
+static void SpanDerived(object[] value) { }
+
+static void ArrayDerived(Span<object> value) { }
+//static void ArrayDerived(string[] value) { }
+
+SpanDerived(["", ""]);
+
+
+Wookiee[] list1 = [new(1, "test1", 1), new(2, "test2", 2)];
+Wookiee[] list2 = [new(3, "test3", 3), new(4, "test4", 4)];
+
+Wookiee[] listFinal = [.. list1, .. list2];
+
+Console.WriteLine("nb : {0}", Wookiee.nb);
+
--- dotnet8/whatsnew/Csharp12/discoverdotnet8/discoverdotnet8/Wookiee.cs
+++ dotnet8/whatsnew/Csharp12/discoverdotnet8/discoverdotnet8/Wookiee.cs
@@ -4,6 +4,13 @@
 
     internal class Wookiee(int id, string name)
     {
+        public static int nb = 0;
+
+        public Wookiee(int id, string name, int size) : this(id, name)
+        {
+            nb++;
+        }
+
         public string Name { get; set; } = name;
 
         public IEnumerable<int> MaxPositions => [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
