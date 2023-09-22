f6a671de2a39ce8ed5d99d993e3b5116bb891962
Evan BOISSONNOT <evan@boissonnot.fr>
2023-09-22 18:25:53 +0200

    feat: filter minimal api

---
+++ dotnet7/asp.net core seven/TestsNouveautésDotnet7/TestFilterMinimalApi/Filters/LogMoreFilter.cs
@@ -0,0 +1,25 @@
+﻿namespace TestFilterMinimalApi.Filters
+{
+    public class LogMoreFilter : IEndpointFilter
+    {
+        protected readonly ILogger Logger;
+        private readonly string _methodName;
+
+        public LogMoreFilter(ILoggerFactory loggerFactory)
+        {
+            LoggerFactory = loggerFactory;
+            Logger = loggerFactory.CreateLogger<LogMoreFilter>();
+            _methodName = GetType().Name;
+        }
+
+        public ILoggerFactory LoggerFactory { get; }
+
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+            Logger.LogInformation("{MethodName} Before next", _methodName);
+            var result = await next(context);
+            Logger.LogInformation("{MethodName} After next", _methodName);
+            return result;
+        }
+    }
+}
---
+++ dotnet7/asp.net core seven/TestsNouveautésDotnet7/TestFilterMinimalApi/Program.cs
@@ -0,0 +1,68 @@
+using TestFilterMinimalApi.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+
+
+app.MapGet("/weatherforecast/{id}", (string id) =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.AddEndpointFilter(async (context, next) =>
+{
+    if (context.GetArgument<string>(0) == "11")
+    {
+        return Results.Problem();
+    }
+
+    return await next(context);
+})
+.AddEndpointFilter(async (context, next) =>
+{
+    app.Logger.LogInformation("Before filter");
+
+    var result = await next(context);
+
+    app.Logger.LogInformation("After filter");
+
+    return result;
+})
+.AddEndpointFilter<LogMoreFilter>()
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
