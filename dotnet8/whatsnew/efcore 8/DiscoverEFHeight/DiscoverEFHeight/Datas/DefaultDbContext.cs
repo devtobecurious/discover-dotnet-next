d9f21456fcc66ca2909766a438dd4da7a3aa46c8
Evan BOISSONNOT <evan@boissonnot.fr>
2023-09-29 01:01:03 +0200

    feat: request json table

---
+++ dotnet8/whatsnew/efcore 8/DiscoverEFHeight/DiscoverEFHeight/Datas/DefaultDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DiscoverEFHeight.Datas
+{
+    public class DefaultDbContext : DbContext
+    {
+        public DefaultDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected DefaultDbContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().OwnsOne(
+                author => author.Contact, ownedNavigationBuilder =>
+                {
+                    ownedNavigationBuilder.ToJson();
+                    ownedNavigationBuilder.OwnsOne(contactDetails => contactDetails.Address);
+                });
+
+
+        }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}
